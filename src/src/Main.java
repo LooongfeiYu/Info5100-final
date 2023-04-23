90769dd62f41815e98e1fe1c3ceee683fa9056bb
LooongfeiYu <123100702+LooongfeiYu@users.noreply.github.com>
2023-04-22 22:05:35 -0400

    name changed

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package info5100.pkgfinal;
+package src;
 
 /**
  *
  * @author yulon
  */
-public class Info5100Final {
+public class Main {
 
     /**
      * @param args the command line arguments

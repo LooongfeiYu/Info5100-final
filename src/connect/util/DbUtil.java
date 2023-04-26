94acca50252906e59889cf4bc3f317c3c39b43f5
LooongfeiYu <123100702+LooongfeiYu@users.noreply.github.com>
2023-04-25 22:45:43 -0400

    three classes created

---
+++ src/connect/util/DbUtil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connect.util;
+
+/**
+ *
+ * @author yulon
+ */
+public class DbUtil {
+    
+    private String dbUrl = "jdbc:mysql://localhost:3306/new_database";
+    private String dbUserName = "root";
+    private String dbPassword = "Bran@123";
+    private String jdbcName = "com.mysql.jdbc.Driver";
+    
+}
---
+++ src/model/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author yulon
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
